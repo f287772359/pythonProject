4c0e3bf10c37e57a9ce76c5a346f5bc04ae11fb8
f287772359 <xin02091205@foxmail.com>
2021-03-23 10:33:23 +0800

    加入list，tuple，set“

--- varyadd.py
+++ varyadd.py
@@ -1,3 +1,5 @@
+import sys
+
 def add(*args):   #加法
     total = 0
     for val in args:     #将每个变量取出来
@@ -54,7 +56,22 @@ def zfc():
     print('{0} * {1} = {2}'.format(a, b, a * b))
     print(f'{a} * {b} = {a * b}')
 
-def main():
+def str_cut():
+    fruits = ['grape', 'apple', 'strawberry', 'waxberry']
+    fruits += ['pitaya', 'pear', 'mango']
+    # 列表切片
+    fruits2 = fruits[1:4]
+    print(fruits2)  # apple strawberry waxberry   从第一个到第三个
+    # 可以通过完整切片操作来复制列表
+    fruits3 = fruits[:]
+    print(fruits3)  # ['grape', 'apple', 'strawberry', 'waxberry', 'pitaya', 'pear', 'mango']
+    fruits4 = fruits[-3:-1]
+    print(fruits4)  # ['pitaya', 'pear']  从倒数第三个到倒数第二个
+    # 可以通过反向切片操作来获得倒转后的列表的拷贝
+    fruits5 = fruits[::-1]
+    print(fruits5)  # ['mango', 'pear', 'pitaya', 'waxberry', 'strawberry', 'apple', 'grape']
+
+def list_cut():
     list1 = [1, 3, 5, 7, 100]
     print(list1)  # [1, 3, 5, 7, 100]
     # 乘号表示列表元素的重复
@@ -79,6 +96,121 @@ def main():
         # 通过enumerate函数处理列表之后再遍历可以同时获得元素索引和值
     for index, elem in enumerate(list1):
         print(index, elem)
+
+def list_rank():
+    list1 = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
+    list2 = sorted(list1)
+    # sorted函数返回列表排序后的拷贝不会修改传入的列表
+    # 函数的设计就应该像sorted函数一样尽可能不产生副作用
+    list3 = sorted(list1, reverse=True)
+    # 通过key关键字参数指定根据字符串长度进行排序而不是默认的字母表顺序
+    list4 = sorted(list1, key=len)
+    print(list1)
+    print(list2)
+    print(list3)
+    print(list4)
+    # 给列表对象发出排序消息直接在列表对象上进行排序
+    list1.sort(reverse=True)
+    print(list1)
+
+def fib(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+        yield a
+
+def list_generate():
+    f = [x for x in range(1, 10)]
+    print(f)
+    f = [x + y for x in 'ABCDE' for y in '1234567']
+    print(f)
+    # 用列表的生成表达式语法创建列表容器
+    # 用这种语法创建列表之后元素已经准备就绪所以需要耗费较多的内存空间
+    f = [x ** 2 for x in range(1, 1000)]
+    print(sys.getsizeof(f))  # 查看对象占用内存的字节数
+    print(f)
+    # 请注意下面的代码创建的不是一个列表而是一个生成器对象
+    # 通过生成器可以获取到数据但它不占用额外的空间存储数据
+    # 每次需要数据的时候就通过内部的运算得到数据(需要花费额外的时间)
+    f = (x ** 2 for x in range(1, 1000))
+    print(sys.getsizeof(f))  # 相比生成式生成器不占用存储数据的空间
+    print(f)
+    for val in f:
+        print(val)
+
+def tuple_cut():
+    # 定义元组
+    t = ('骆昊', 38, True, '四川成都')
+    print(t)
+    # 获取元组中的元素
+    print(t[0])
+    print(t[3])
+    # 遍历元组中的值
+    for member in t:
+        print(member)
+    # 重新给元组赋值
+    # t[0] = '王大锤'  # TypeError   不能单独修改元素，修改时必须整体重新赋值
+    # 变量t重新引用了新的元组原来的元组将被垃圾回收
+    t = ('王大锤', 20, True, '云南昆明')
+    print(t)
+    # 将元组转换成列表
+    person = list(t)
+    print(person)
+    # 列表是可以修改它的元素的
+    person[0] = '李小龙'
+    person[1] = 25
+    print(person)
+    # 将列表转换成元组
+    fruits_list = ['apple', 'banana', 'orange']
+    fruits_tuple = tuple(fruits_list)
+    print(fruits_tuple)
+
+def set_change():
+    # 创建集合的字面量语法
+    set1 = {1, 2, 3, 3, 3, 2}
+    print(set1)
+    # 集合中重复的数只显示一次
+    print('Length =', len(set1))
+    # 创建集合的构造器语法(面向对象部分会进行详细讲解)
+    set2 = set(range(1, 10))
+    set3 = set((1, 2, 3, 3, 2, 1))
+    # set只能有一个元素，所以必要再加一层括号
+    print(set2, set3)
+    # 创建集合的推导式语法(推导式也可以用于推导集合)
+    set4 = {num for num in range(1, 100) if num % 3 == 0 or num % 5 == 0}
+    print(set4)
+    set1.add(4)
+    set1.add(5)
+    set2.update([11, 12])
+    set2.discard(5)
+    if 4 in set2:
+        set2.remove(4)
+    print(set1, set2)
+    print(set3.pop())   # 删除第0元素，并输出
+    print(set3)
+
+    # 集合的交集、并集、差集、对称差运算
+    print(set1 & set2)
+    # print(set1.intersection(set2))
+    print(set1 | set2)
+    # print(set1.union(set2))
+    print(set1 - set2)
+    # print(set1.difference(set2))
+    print(set1 ^ set2)  # !(set1&set2)
+    # print(set1.symmetric_difference(set2))
+    # 判断子集和超集
+    print(set2 <= set1)
+    # print(set2.issubset(set1))
+    print(set3 <= set1)
+    # print(set3.issubset(set1))
+    print(set1 >= set2)
+    # print(set1.issuperset(set2))
+    print(set1 >= set3)
+    # print(set1.issuperset(set3))
+
+
+def main():
+    set_change()
 
 if __name__ == '__main__':
     main()

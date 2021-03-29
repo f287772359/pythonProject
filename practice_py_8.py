ded1333636848a6b2406776fd45042bba592178b
f287772359 <xin02091205@foxmail.com>
2021-03-29 16:36:51 +0800

    0329

--- practice_py_8.py
+++ practice_py_8.py
@@ -1,4 +1,5 @@
 from time import sleep
+from math import sqrt
 
 class Student(object):
     # __init__初始化
@@ -40,6 +41,27 @@ class Clock(object):
         return '%02d:%02d:%02d' % \
                (self._hour,self._min,self._second)
 
+class Point(object):
+
+    def __init__(self,x=0,y=0):
+        self.x = x
+        self.y = y
+
+    def move_to(self,x,y):
+        self.x = x
+        self.y = y
+
+    def move_by(self,x,y):
+        self.x += x
+        self.y += y
+
+    def distance_to(self,other):
+        dx = self.x - other.x
+        dy = self.y - other.y
+        return sqrt(dx ** 2 + dy ** 2)
+
+    def __str__(self):
+        return '(%s,%s)' %(str(self.x),str(self.y))
 
 
 def main():
@@ -50,11 +72,18 @@ def main():
     # stu2 = Student('大林',16)
     # stu2.study('相声')
     # stu2.watch_movie()
-    clock = Clock(23,59,58)
-    while True:
-        print(clock.display())
-        sleep(1)
-        clock.run()
+    # clock = Clock(23,59,58)
+    # while True:
+    #    print(clock.display())
+    #    sleep(1)
+    #    clock.run()
+    p1 = Point(3,5)
+    p2 = Point()
+    print(p1)
+    print(p2)
+    p2.move_by(-1,2)
+    print(p2)
+    print(p1.distance_to(p2))
 
 if __name__ == '__main__':
     main()
